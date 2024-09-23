46037902a7c06cbfac851f6948239231a47dac16
Thomas Kowalski <thomas.kowalski@datadoghq.com>
2024-09-23 12:05:31 +0400

    Initial Commit

---
+++ EnglishWords/EnglishWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishWordsApp.swift
+//  EnglishWords
+//
+//  Created by Thomas Kowalski on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct EnglishWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EnglishWordsTests/EnglishWordsTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishWordsTests.swift
+//  EnglishWordsTests
+//
+//  Created by Thomas Kowalski on 23/09/2024.
+//
+
+import Testing
+@testable import EnglishWords
+
+struct EnglishWordsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
